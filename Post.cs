c8731e203b5a45f24e08ac93e6f8cbfb6eb922b9
Daniil Sviridov <daniil_sviridov@mail.ru>
2022-04-15 12:56:39 +0300

    Add project files.

---
+++ Post.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    internal class Post
+    {  
+        public int UserId { get; set; }
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string Body { get; set; }
+
+        public override string ToString() => $"UserId:{UserId}\nId:{Id}\nTitle:{Title}\nBody:{Body}\n";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using Lesson1;
+using System.Text.Json;
+
+async Task<Post> GetPost(HttpClient client, int id)
+{
+
+    var response = await client.GetAsync($"https://jsonplaceholder.typicode.com/posts/{id}");
+
+    if (response.IsSuccessStatusCode)
+    {
+        await using var responseStream = await response.Content.ReadAsStreamAsync();
+        var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+
+        if (await JsonSerializer.DeserializeAsync<Post>(responseStream, options) is Post post)
+            return post;
+        else
+            throw new Exception($"Error content");
+    }
+    else
+    {
+        throw new Exception($"Error code {response.StatusCode}");
+    }
+}
+// See https://aka.ms/new-console-template for more information
+
+using HttpClient client = new();
+    {
+    client.BaseAddress = new Uri("https://jsonplaceholder.typicode.com");
+
+    var tasks = new List<Task<Post>>();
+    for (int i = 4; i <= 13; i++) tasks.Add(GetPost(client, i));
+
+    Task.WaitAll(tasks.ToArray());
+
+    using (var sw = new StreamWriter(File.Create("result.txt")))
+    {
+        foreach (var task in tasks)
+            if (task.IsCompleted && task.Exception == null)
+                sw.WriteLine(task.Result);
+    }
+
+}
